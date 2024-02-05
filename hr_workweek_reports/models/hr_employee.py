26f05815c141b9233c4c60b689fbb35268ad42c6
Carol <carolina@domatix.com>
2024-02-05 14:26:17 +0100

    [MIG] hr_workweek_reports: migration to 16

--- hr_workweek_reports/models/hr_employee.py
+++ hr_workweek_reports/models/hr_employee.py
@@ -7,16 +7,6 @@ class HrEmployee(models.Model):
     @api.model
     def send_weekly_report_email(self):
         ir_default = self.env["ir.default"].sudo()
-        fields = [
-            "subject",
-            "body_html",
-            "email_from",
-            "email_to",
-            "partner_to",
-            "email_cc",
-            "reply_to",
-            "scheduled_date",
-        ]
         if ir_default.get("res.config.settings", "send_mail_notification"):
             excluded_calendars = (
                 ir_default.get("res.config.settings", "excluded_calendar_ids") or []
@@ -26,22 +16,11 @@ class HrEmployee(models.Model):
                 for employee in self.env["hr.employee"].search(
                     [("resource_calendar_id", "not in", excluded_calendars)]
                 ):
-                    mail = template.generate_email([employee.id], fields)
-                    self.env["mail.mail"].create(mail.values()).send()
+                    template.send_mail(employee.id, force_send=True)
 
     @api.model
     def send_weekly_summary_report_email(self):
         ir_default = self.env["ir.default"].sudo()
-        fields = [
-            "subject",
-            "body_html",
-            "email_from",
-            "email_to",
-            "partner_to",
-            "email_cc",
-            "reply_to",
-            "scheduled_date",
-        ]
         if ir_default.get("res.config.settings", "send_mail_notification"):
             excluded_calendars = (
                 ir_default.get("res.config.settings", "excluded_calendar_ids") or []
@@ -72,15 +51,9 @@ class HrEmployee(models.Model):
                     ]
                 )
                 for allowed_employee in allowed_employees:
-                    mail = template.with_context(
-                        **{
-                            "employee": self.env["hr.employee"],
-                            "calendars": allowed_calendars,
-                        }
-                    ).generate_email(allowed_employee.id, fields)
-
-                    mail["email_from"] = (
-                        self.env["hr.employee"]
+                    template.with_context(calendars=allowed_calendars).send_mail(allowed_employee.id, force_send=True, email_values={
+                        'email_to': allowed_employee.work_email,
+                        'email_from': self.env["hr.employee"]
                         .browse(
                             [
                                 ir_default.get(
@@ -88,7 +61,5 @@ class HrEmployee(models.Model):
                                 )
                             ]
                         )
-                        .work_email
-                    )
-                    mail["email_to"] = allowed_employee.work_email
-                    self.env["mail.mail"].create(mail).send()
+                        .work_email,
+                    })
